7be6f2e53cde97dd78c56db74e9d760a90bda30d
Rblzhuk <shilnikovdmitrievich@yandex.ru>
2023-12-03 20:46:36 +0300

    Считает

---
+++ src/main/java/Main/javacalculator/MainController.java
@@ -0,0 +1,13 @@
+package com.example.javacalculator;
+
+import javafx.scene.control.Button;
+
+public class MainController {
+    public Button button_1;
+
+
+
+    public void onButton_1Click(){
+
+    }
+}
